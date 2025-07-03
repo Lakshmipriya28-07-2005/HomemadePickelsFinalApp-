7634b36e71e4f580a27527f8f9b1dd0a88d77e81
Lakshmipriya28-07-2005 <lakshmipriyavinukonda@gmail.com>
2025-07-03 13:13:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -207,5 +207,5 @@ def success():
     return render_template('success.html')
 
 # Run the application
-if __name__ == '__main__':
-    app.run(debug=True)
+if _name_ == '_main_':
+    app.run(debug=True, host='0.0.0.0', port=5000)
